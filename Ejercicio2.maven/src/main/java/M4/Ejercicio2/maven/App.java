b1303cc12dc0d3dba02642b0f3eb96e533a3d2ba
Chals <csanchezbasket@gmail.com>
2022-10-28 13:16:34 +0200

    Commit inicial proyecto Maven M4

---
+++ Ejercicio2.maven/src/main/java/M4/Ejercicio2/maven/App.java
@@ -0,0 +1,13 @@
+package M4.Ejercicio2.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
